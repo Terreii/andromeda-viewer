29c0bd6f6e2ab928ea481e3f4a844ce0a9b7dc7f
Christopher Astfalk <christopher.astfalk@gmail.com>
2015-08-17 17:01:19 +0000

    Some style for the login screen

--- main.js
+++ main.js
@@ -6,7 +6,32 @@
  */
 
 var viewerInfo = require('./js/viewerInfo');
+var parseFullName = require('./js/avatarName');
 
+function displayLoginError (message) {
+  var messageDisplay = document.getElementById('loginErrorMessage');
+  messageDisplay.textContent = message.toString();
+  messageDisplay.style.display = 'block';
+}
+
+// Show the name of the Viewer
 document.title = viewerInfo.name;
+document.getElementById('loginViewerName').textContent = viewerInfo.name;
 
-document.getElementById('loginButton').disabled = false;
+var button = document.getElementById('loginButton');
+
+// Login
+button.addEventListener('click', function (event) {
+  var loginName = document.getElementById('loginName').value;
+  var password = document.getElementById('loginPassword').value;
+
+  if (loginName.length === 0 || password.length === 0) {
+    displayLoginError('Please insert a name and a password');
+    return;
+  }
+
+  var userName = parseFullName(loginName);
+  console.log(userName);
+});
+
+button.disabled = false;
