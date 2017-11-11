8b01b346d861c0ee66e330a246ea5565e80c1b8e
Christopher Astfalk <christopher.astfalk@icloud.com>
2017-11-11 01:13:03 +0100

    refactor: reduxify chatBox and topBar

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import styled from 'styled-components'
 
 import { logout } from '../session'
-import State from '../store/state'
 import { showSignOutPopup, showSignInPopup } from '../actions/viewerAccount'
 
 const MenuBar = styled.div`
+  z-index: 100;
   top: 0em;
   left: 0em;
   width: 100vw;
@@ -48,31 +49,13 @@ const AccountMenuBody = styled.div`
   padding: .6em;
 `
 
-export default class TopBar extends React.Component {
+class TopBar extends React.Component {
   constructor () {
     super()
     this.state = {
-      account: State.getState().account,
-      savedAvatars: [],
       showAccountMenu: false
     }
     this._boundToggleMenu = this._toggleAccountMenu.bind(this)
-  }
-
-  componentDidMount () {
-    this._unsubscribe = State.subscribe(this._onChange.bind(this))
-  }
-
-  componentWillUnmount () {
-    this._unsubscribe()
-  }
-
-  _onChange () {
-    const account = State.getState().account
-    if (account === this.state.account) return
-    this.setState({
-      account
-    })
   }
 
   _logout (event) {
@@ -82,53 +65,70 @@ export default class TopBar extends React.Component {
 
   _logoutFromViewer (event) {
     event.preventDefault()
-    State.dispatch(showSignOutPopup())
+    this.props.showSignOutPopup()
   }
 
   _toggleAccountMenu (event) {
+    if (this.state.showAccountMenu) return
+
+    setTimeout(() => window.addEventListener('click', event => {
+      this.setState({
+        showAccountMenu: false
+      })
+    }, {
+      once: true
+    }), 10)
     this.setState({
-      showAccountMenu: !this.state.showAccountMenu
+      showAccountMenu: true
     })
   }
 
   _showSignInPopup (event) {
     event.preventDefault()
-    State.dispatch(showSignInPopup())
+    this.props.showSignInPopup()
   }
 
   _showSignUpPopup (event) {
     event.preventDefault()
-    State.dispatch(showSignInPopup('signUp'))
+    this.props.showSignInPopup('signUp')
   }
 
   renderAccountMenu () {
     if (!this.state.showAccountMenu) return null
-    const isLoggedIn = this.state.account.get('loggedIn')
+    const isLoggedIn = this.props.account.get('loggedIn')
+
     const greeting = isLoggedIn
-      ? `Hello ${this.state.account.get('avatarName')}`
+      ? `Hello ${this.props.account.get('avatarName')}`
       : ''
-    const viewerAccountLoggedIn = this.state.account.getIn([
+
+    const viewerAccountLoggedIn = this.props.account.getIn([
       'viewerAccount',
       'loggedIn'
     ])
+
     const viewerAccountText = viewerAccountLoggedIn
-      ? `Hello ${this.state.account.getIn(['viewerAccount', 'username'])}`
-      : <a href='#signin' onClick={this._showSignInPopup}>Sign into Andromeda</a>
+      ? `Hello ${this.props.account.getIn(['viewerAccount', 'username'])}`
+      : <a href='#signin' onClick={this._showSignInPopup.bind(this)}>Sign into Andromeda</a>
+
     return <AccountMenuBody>
       <div>{greeting}</div>
+
       <div>
         {viewerAccountText}
       </div>
+
       <div style={{display: viewerAccountLoggedIn ? 'none' : ''}}>
-        <a href='#signup' onClick={this._showSignUpPopup}>Sign up to Andromeda</a>
+        <a href='#signup' onClick={this._showSignUpPopup.bind(this)}>Sign up to Andromeda</a>
       </div>
+
       <div style={{display: isLoggedIn ? '' : 'none'}}>
         <LogoutButton href='#' onClick={this._logout}>
           log out
         </LogoutButton>
       </div>
+
       <div style={{display: viewerAccountLoggedIn ? '' : 'none'}}>
-        <Link href='' onClick={this._logoutFromViewer}>
+        <Link href='' onClick={this._logoutFromViewer.bind(this)}>
           Log out from Viewer
         </Link>
       </div>
@@ -159,3 +159,16 @@ export default class TopBar extends React.Component {
     </MenuBar>
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    account: state.account
+  }
+}
+
+const mapDispatchToProps = {
+  showSignOutPopup,
+  showSignInPopup
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopBar)
