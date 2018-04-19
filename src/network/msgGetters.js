fdee31ec1883f8cdc7f0bee084ceb2d5bfd8f0c2
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-04-19 17:40:07 +0200

    test(network): test network msg to new getter functions
    Test the NetworkMessages with the new helper functions.

---
+++ src/network/msgGetters.js
@@ -0,0 +1,13 @@
+// Helper functions to access values in network messages
+
+export function getValueOf () {}
+
+export function getStringValueOf () {}
+
+export function getValuesOf () {}
+
+export function getStringValuesOf () {}
+
+export function getNumberOfBlockInstancesOf () {}
+
+export function mapBlockOf () {}
