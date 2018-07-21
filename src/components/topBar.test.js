926eda7a726851a1444f0678656127d4154003a9
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-07-21 13:17:38 +0200

    test: add test for topBar

---
+++ src/components/topBar.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import Immutable from 'immutable'
+
+import TopBar from './topBar'
+
+test('renders without crashing', () => {
+  const account = Immutable.fromJS({
+    avatarName: '',
+    loggedIn: false,
+    viewerAccount: {
+      loggedIn: false,
+      username: '',
+      signInPopup: ''
+    }
+  })
+  const noMessage = shallow(<TopBar
+    account={account}
+  />)
+
+  expect(noMessage).toContainReact(<span>Welcome</span>)
+
+  const messageOfTheDay = Immutable.Map({
+    text: 'Hi there! Please go to: ',
+    href: 'https://example.com'
+  })
+
+  const withMessage = shallow(<TopBar
+    account={account}
+    messageOfTheDay={messageOfTheDay}
+  />)
+
+  expect(withMessage.find('span span').first().text())
+    .toBe('Message of the day:Hi there! Please go to: <styled.a />')
+})
