e396e65d083756c5ea128063ab8f29f85658d960
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-09-28 15:50:59 +0200

    style: add css style to loading chat compoent

--- src/containers/App.js
+++ src/containers/App.js
@@ -42,9 +42,19 @@ const AppContainer = styled.div`
   margin: 0px;
 `
 
+const LoadingView = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.5);
+`
+
 const LoadableChatComponent = Loadable({
   loader: () => import('./chatContainer'),
-  loading: () => <div>Loading ...</div>
+  loading: () => <LoadingView>
+    <span>Loading ...</span>
+  </LoadingView>
 })
 
 class App extends React.Component {
