690cd427ab008cfe90125847fa86c070bf9778e1
Christopher Astfalk <christopher.astfalk@icloud.com>
2018-10-28 19:48:51 +0100

    refactor(app.js): rename App.js to app.js

--- src/containers/root.js
+++ src/containers/root.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
-import App from './App'
+
+import App from './app'
 
 const Root = ({ store }) => {
   return <Provider store={store}>
