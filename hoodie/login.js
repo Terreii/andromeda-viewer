e3e63ea5b28218bc8acceb906e8e4e29301c11a1
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-01-25 18:16:26 +0100

    refactor(login): send all viewer info in viewerData object
    All data from the viewer, will be send in an viewerData object, in the login request body.

--- hoodie/login.js
+++ hoodie/login.js
@@ -22,10 +22,12 @@ function processLogin (request, reply) {
   getMacAddress(request)
     .then(function (mac) {
       const reqData = request.payload
+      const viewerData = reqData.viewerData // special data for the viewer
+      delete reqData.viewerData // that shouldn't be send to the grid
 
-      var loginURL
-      if (reqData.grid && typeof reqData.grid.url === 'string') {
-        loginURL = url.parse(reqData.grid.url)
+      let loginURL
+      if (viewerData.grid && typeof viewerData.grid.url === 'string') {
+        loginURL = url.parse(viewerData.grid.url)
       } else {
         loginURL = {
           host: 'login.agni.lindenlab.com',
@@ -37,24 +39,17 @@ function processLogin (request, reply) {
         reply(400)
         return
       }
-      var xmlrpcClient = loginURL.protocol == null || loginURL.protocol === 'https:'
+      const xmlrpcClient = loginURL.protocol == null || loginURL.protocol === 'https:'
         ? xmlrpc.createSecureClient(loginURL)
         : xmlrpc.createClient(loginURL) // osgrid uses http for login ... why??
 
       reqData.mac = mac // adding the needed mac-address
 
-      ;[
-        'grid',
-        'viewerUserId'
-      ].forEach(key => {
-        reqData[key] = undefined
-      })
-
       xmlrpcClient.methodCall('login_to_simulator', [reqData], (err, data) => {
         if (err) {
           reply(err)
         } else {
-          var response = reply(undefined, data)
+          const response = reply(undefined, data)
           response.type('application/json')
         }
       })
@@ -66,13 +61,14 @@ function processLogin (request, reply) {
 
 async function getMacAddress (request) {
   const payload = request.payload
+  const viewerData = payload.viewerData
 
   // If it is a logged in user
-  if ('viewerUserId' in payload) {
+  if ('userId' in viewerData) {
     const accounts = request.server.plugins.account.api.accounts
 
     try {
-      const user = await accounts.find(payload.viewerUserId, { include: 'profile' })
+      const user = await accounts.find(viewerData.userId, { include: 'profile' })
 
       if (
         user.profile != null &&
@@ -82,7 +78,7 @@ async function getMacAddress (request) {
         return user.profile.mac
       } else {
         // Add a mac-address to the user
-        const updated = await accounts.update(payload.viewerUserId, user => {
+        const updated = await accounts.update(viewerData.userId, user => {
           let mac
 
           do {
