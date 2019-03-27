a7314efe6cca6f6ee821200573588478f5247190
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-03-27 03:41:13 +0100

    test(resetKeysPopup): add tests for popup displaying reset-keys
    The reset-keys are for resetting the encryption password, when the user forgets their password.

--- src/components/popups/index.test.js
+++ src/components/popups/index.test.js
@@ -48,6 +48,33 @@ test('renders signUp without crashing', () => {
   expect(rendered.find('h4').text()).toBe('Sign up')
 })
 
+test('renders resetKeys without crashing', () => {
+  global.URL.createObjectURL = jest.fn()
+  global.URL.revokeObjectURL = jest.fn()
+
+  const rendered = mount(
+    <PopupRenderer
+      popup='resetKeys'
+      unlock={() => {}}
+      signOut={() => {}}
+      data={[
+        '6f9be4b69637f3e18a0f747c4ae70158',
+        '1d8ad0e879644a3cff07134a4b50bd09',
+        '52f4944503133cd658cefbe5c905ba56',
+        '8a04bce4d35319db6b54d9b109f6fd2e',
+        '0f185fb339d36d635e58fc6c6be85edd',
+        '1e34712bd7734c489a8e20e3212146e1',
+        '74e7098dbc5a0cfaa7ab595cf4d970b7',
+        'd6acf8385d77e16423a75a1b5a3a883c',
+        '20050f0a104c0f5dcafcf8798f2d130e',
+        '5d588ce0ee8de2b80aab0aaad8e57b38'
+      ]}
+    />
+  )
+
+  expect(rendered.find('h4').text()).toBe('Password reset keys')
+})
+
 test('renders signOut without crashing', () => {
   const rendered = mount(
     <PopupRenderer
---
+++ src/components/popups/resetKeysPopup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Popup from './popup'
+
+export default function ResetKeysPopup ({ resetKeys, onClose }) {
+  return <Popup title='Password reset keys' onClose={onClose}>
+    <div />
+  </Popup>
+}
