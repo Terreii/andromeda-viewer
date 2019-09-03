a0708efc16a661d50784b777260f31a8e96c7e8a
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-09-03 13:05:26 +0200

    refactor(chatDialog): use form and onSubmit for text input

--- src/components/chatDialog.test.js
+++ src/components/chatDialog.test.js
@@ -47,7 +47,7 @@ test('renders local chat', () => {
       value: 'Hello World!'
     }
   })
-  sendButton.simulate('click')
+  sendButton.simulate('submit')
 
   expect(sendData.text).toBe('Hello World!')
   expect(sendData.count).toBe(1)
