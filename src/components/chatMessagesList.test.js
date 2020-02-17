3ae64d6e4fba7727eed548a4eb979d898603522c
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-02-17 20:54:27 +0100

    test(chatMessagesList): refactor tests to use react-testing-library

--- src/components/chatMessagesList.test.js
+++ src/components/chatMessagesList.test.js
@@ -1,152 +1,144 @@
 import { axe } from 'jest-axe'
 import React from 'react'
-import { mount } from 'enzyme'
+import { render } from '@testing-library/react'
 
 import ChatMessagesList from './chatMessagesList'
 import AvatarName from '../avatarName'
 
-test('renders local chat without crashing', () => {
-  const messages = [
-    {
-      _id: 'first',
-      fromId: 'ABCB',
-      message: 'Hello world!',
-      time: '2018-08-10T11:03:00.000Z'
-    },
-    {
-      _id: 'second',
-      fromId: '1234',
-      message: 'How are you?',
-      time: '2018-08-10T11:03:32.734Z'
+describe('local chat', () => {
+  it('renders without crashing', () => {
+    const messages = [
+      {
+        _id: 'first',
+        fromId: 'ABCB',
+        message: 'Hello world!',
+        time: '2018-08-10T11:03:00.000Z'
+      },
+      {
+        _id: 'second',
+        fromId: '1234',
+        message: 'How are you?',
+        time: '2018-08-10T11:03:32.734Z'
+      }
+    ]
+
+    const names = {
+      ABCB: new AvatarName('Testery MacTestface'),
+      1234: new AvatarName('Viewerer Account')
     }
-  ]
 
-  const names = {
-    ABCB: new AvatarName('Testery MacTestface'),
-    1234: new AvatarName('Viewerer Account')
-  }
+    const { queryByText } = render(<ChatMessagesList
+      messages={messages}
+      names={names}
+    />)
 
-  const rendered = mount(<ChatMessagesList
-    messages={messages}
-    names={names}
-  />)
+    expect(queryByText('13:03:00')).toBeTruthy()
+    expect(queryByText('13:03:00').tagName).toBe('TIME')
+    expect(queryByText('Testery Mactestface:')).toBeTruthy()
+    expect(queryByText('Hello world!')).toBeTruthy()
 
-  expect(rendered.find('div').length).toBe(3)
-
-  const timeElements = rendered.find('time')
-  expect(timeElements.length).toBe(2)
-  timeElements.forEach(element => {
-    const text = element.text()
-    expect(text).toMatch(new RegExp('\\d\\d:\\d\\d:\\d\\d'))
+    expect(queryByText('13:03:32')).toBeTruthy()
+    expect(queryByText('13:03:32').tagName).toBe('TIME')
+    expect(queryByText('Viewerer Account:')).toBeTruthy()
+    expect(queryByText('How are you?')).toBeTruthy()
   })
 
-  const messageTexts = rendered.find('span.messageText')
-  expect(messageTexts.length).toBe(2)
-  messageTexts.forEach((element, index) => {
-    const text = element.text()
-    expect(text).toBe(index === 0 ? 'Hello world!' : 'How are you?')
-  })
-})
+  it('should pass aXe', async () => {
+    const messages = [
+      {
+        _id: 'first',
+        fromId: 'ABCB',
+        message: 'Hello world!',
+        time: '2018-08-10T11:03:00.000Z'
+      },
+      {
+        _id: 'second',
+        fromId: '1234',
+        message: 'How are you?',
+        time: '2018-08-10T11:03:32.734Z'
+      }
+    ]
 
-test('renders IM chat without crashing', () => {
-  const messages = [
-    {
-      _id: 'first',
-      fromId: 'ABCB',
-      message: 'Hello world!',
-      time: '2018-08-10T11:03:00.000Z'
-    },
-    {
-      _id: 'second',
-      fromId: '1234',
-      message: 'How are you?',
-      time: '2018-08-10T11:03:32.734Z'
+    const names = {
+      ABCB: new AvatarName('Testery MacTestface'),
+      1234: new AvatarName('Viewerer Account')
     }
-  ]
 
-  const names = {
-    ABCB: new AvatarName('Testery MacTestface'),
-    1234: new AvatarName('Viewerer Account')
-  }
+    const { container } = render(<ChatMessagesList
+      messages={messages}
+      names={names}
+    />)
 
-  const rendered = mount(<ChatMessagesList
-    messages={messages}
-    isIM
-    names={names}
-  />)
-
-  expect(rendered.find('div').length).toBe(3)
-
-  const timeElements = rendered.find('time')
-  expect(timeElements.length).toBe(2)
-  timeElements.forEach(element => {
-    const text = element.text()
-    expect(text).toMatch(new RegExp('\\d\\d:\\d\\d:\\d\\d'))
-  })
-
-  const messageTexts = rendered.find('span.messageText')
-  expect(messageTexts.length).toBe(2)
-  messageTexts.forEach((element, index) => {
-    const text = element.text()
-    expect(text).toBe(index === 0 ? 'Hello world!' : 'How are you?')
+    expect(await axe(container)).toHaveNoViolations()
   })
 })
 
-test('Local chat should pass aXe', async () => {
-  const messages = [
-    {
-      _id: 'first',
-      fromId: 'ABCB',
-      message: 'Hello world!',
-      time: '2018-08-10T11:03:00.000Z'
-    },
-    {
-      _id: 'second',
-      fromId: '1234',
-      message: 'How are you?',
-      time: '2018-08-10T11:03:32.734Z'
+describe('IMs', () => {
+  it('renders without crashing', () => {
+    const messages = [
+      {
+        _id: 'first',
+        fromId: 'ABCB',
+        message: 'Hello world!',
+        time: '2018-08-10T11:03:00.000Z'
+      },
+      {
+        _id: 'second',
+        fromId: '1234',
+        message: 'How are you?',
+        time: '2018-08-10T11:03:32.734Z'
+      }
+    ]
+
+    const names = {
+      ABCB: new AvatarName('Testery MacTestface'),
+      1234: new AvatarName('Viewerer Account')
     }
-  ]
 
-  const names = {
-    ABCB: new AvatarName('Testery MacTestface'),
-    1234: new AvatarName('Viewerer Account')
-  }
+    const { queryByText } = render(<ChatMessagesList
+      messages={messages}
+      isIM
+      names={names}
+    />)
 
-  const rendered = mount(<ChatMessagesList
-    messages={messages}
-    names={names}
-  />)
+    expect(queryByText('13:03:00')).toBeTruthy()
+    expect(queryByText('13:03:00').tagName).toBe('TIME')
+    expect(queryByText('Testery Mactestface:')).toBeTruthy()
+    expect(queryByText('Hello world!')).toBeTruthy()
 
-  expect(await axe(rendered.html())).toHaveNoViolations()
-})
+    expect(queryByText('13:03:32')).toBeTruthy()
+    expect(queryByText('13:03:32').tagName).toBe('TIME')
+    expect(queryByText('Viewerer Account:')).toBeTruthy()
+    expect(queryByText('How are you?')).toBeTruthy()
+  })
 
-test('IM chat should pass aXe', async () => {
-  const messages = [
-    {
-      _id: 'first',
-      fromId: 'ABCB',
-      message: 'Hello world!',
-      time: '2018-08-10T11:03:00.000Z'
-    },
-    {
-      _id: 'second',
-      fromId: '1234',
-      message: 'How are you?',
-      time: '2018-08-10T11:03:32.734Z'
+  it('should pass aXe', async () => {
+    const messages = [
+      {
+        _id: 'first',
+        fromId: 'ABCB',
+        message: 'Hello world!',
+        time: '2018-08-10T11:03:00.000Z'
+      },
+      {
+        _id: 'second',
+        fromId: '1234',
+        message: 'How are you?',
+        time: '2018-08-10T11:03:32.734Z'
+      }
+    ]
+
+    const names = {
+      ABCB: new AvatarName('Testery MacTestface'),
+      1234: new AvatarName('Viewerer Account')
     }
-  ]
 
-  const names = {
-    ABCB: new AvatarName('Testery MacTestface'),
-    1234: new AvatarName('Viewerer Account')
-  }
+    const { container } = render(<ChatMessagesList
+      messages={messages}
+      isIM
+      names={names}
+    />)
 
-  const rendered = mount(<ChatMessagesList
-    messages={messages}
-    isIM
-    names={names}
-  />)
-
-  expect(await axe(rendered.html())).toHaveNoViolations()
+    expect(await axe(container)).toHaveNoViolations()
+  })
 })
