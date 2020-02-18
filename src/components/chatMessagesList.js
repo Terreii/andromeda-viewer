d5113d892e0a4ba67f46bb96ee4bd2d5659792cf
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-02-18 17:52:29 +0100

    feat(text): add text component to detect urls

--- src/components/chatMessagesList.js
+++ src/components/chatMessagesList.js
@@ -1,67 +1,15 @@
-import React, { useMemo, memo } from 'react'
-import anchorme from 'anchorme'
+import React, { memo } from 'react'
 import autoscroll from 'autoscroll-react'
 
 import styles from './chatMessagesList.module.css'
 
+import Text from './text'
+
 const TextLine = memo(({ msg, name }) => {
   const time = new Date(msg.time)
 
-  const message = msg.message
-  const isIrcMe = message.startsWith('/me ') || message.startsWith("/me'")
-
-  const parsed = useMemo(
-    () => {
-      const urls = anchorme(message, { list: true })
-
-      let text = isIrcMe ? message.substring(4) : message
-
-      if (urls.length === 0) return [text]
-      const result = []
-
-      for (const url of urls) {
-        const urlStartIndex = text.indexOf(url.raw)
-        const urlEndIndex = urlStartIndex + url.raw.length
-
-        const hasLinkBody = text.charAt(urlStartIndex - 1) === '[' &&
-          /\s/.test(text.charAt(urlEndIndex))
-
-        let index = urlStartIndex
-        let endIndex = urlEndIndex
-        let linkBody = url.raw
-
-        if (hasLinkBody) {
-          const closingIndex = text.indexOf(']', urlEndIndex + 1)
-          const body = text.substring(urlEndIndex + 1, closingIndex)
-
-          if (closingIndex >= 0 && body.length > 0) {
-            index = urlStartIndex - 1
-            endIndex = closingIndex + 1
-            linkBody = body
-          }
-        }
-
-        // split out text only part
-        if (index > 0) {
-          result.push(text.substring(0, index))
-        }
-
-        result.push({
-          text: linkBody,
-          url: url.raw
-        })
-
-        text = text.substring(endIndex)
-      }
-
-      if (text.length > 0) {
-        result.push(text)
-      }
-
-      return result
-    },
-    [isIrcMe, message]
-  )
+  const isIrcMe = msg.message.startsWith('/me ') || msg.message.startsWith("/me'")
+  const message = isIrcMe ? msg.message.substring(4) : msg.message
 
   return <div className={isIrcMe ? styles.icr_me : styles.Message}>
     <time dateTime={time.toISOString()}>
@@ -75,15 +23,7 @@ const TextLine = memo(({ msg, name }) => {
     <span className={styles.AvatarName}>{name.toString()}{isIrcMe ? '' : ':'} </span>
 
     <span className='messageText'>
-      {parsed.map((part, index) => {
-        if (typeof part === 'string') {
-          return part
-        } else {
-          return <a key={index} href={part.url} target='_blank' rel='noopener noreferrer'>
-            {part.text}
-          </a>
-        }
-      })}
+      <Text text={message} />
     </span>
   </div>
 })
--- src/components/text.js
+++ src/components/text.js
@@ -1,3 +1,84 @@
-export default function Text () {
-  return null
+import React, { useMemo } from 'react'
+import anchorme from 'anchorme'
+
+/**
+ * Render text and detect URLs it. URLs will be rendered as <a>.
+ * This can also use the [url some text] formatting used in SL and openSIM.
+ * @param {object} param             React Argument.
+ * @param {string} param.text        Text body that should be rendered.
+ * @param {string} [param.className] ClassNames for the links (<a>).
+ */
+export default function Text ({ text, className }) {
+  const parsed = useMemo(
+    () => {
+      const urls = anchorme(text, { list: true })
+
+      if (urls.length === 0) {
+        return [text]
+      }
+
+      let rest = text
+      const result = []
+
+      for (const url of urls) {
+        const urlStartIndex = rest.indexOf(url.raw)
+        const urlEndIndex = urlStartIndex + url.raw.length
+
+        const hasLinkBody = rest.charAt(urlStartIndex - 1) === '[' &&
+          /\s/.test(rest.charAt(urlEndIndex))
+
+        let index = urlStartIndex
+        let endIndex = urlEndIndex
+        let linkBody = url.raw
+
+        if (hasLinkBody) {
+          const closingIndex = rest.indexOf(']', urlEndIndex + 1)
+          const body = rest.substring(urlEndIndex + 1, closingIndex)
+
+          if (closingIndex >= 0 && body.length > 0) {
+            index = urlStartIndex - 1
+            endIndex = closingIndex + 1
+            linkBody = body
+          }
+        }
+
+        // split out text only part
+        if (index > 0) {
+          result.push(rest.substring(0, index))
+        }
+
+        result.push({
+          text: linkBody,
+          url: url.raw
+        })
+
+        rest = rest.substring(endIndex)
+      }
+
+      if (rest.length > 0) {
+        result.push(rest)
+      }
+
+      return result
+    },
+    [text]
+  )
+
+  return <>
+    {parsed.map((part, index) => {
+      if (typeof part === 'string') {
+        return part
+      } else {
+        return <a
+          key={index}
+          href={part.url}
+          target='_blank'
+          rel='noopener noreferrer'
+          className={className}
+        >
+          {part.text}
+        </a>
+      }
+    })}
+  </>
 }
