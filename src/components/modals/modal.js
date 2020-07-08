14579eb353b66539baefd4aff3ea61614a9e901d
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-07-08 16:31:20 +0200

    refactor(tailwindcss): move modal to tailwindcss

--- src/components/modals/modal.js
+++ src/components/modals/modal.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import { Portal } from 'reakit/Portal'
 import { Dialog, DialogBackdrop } from 'reakit'
 
 import closeIcon from '../../icons/icon_close.svg'
-import styles from './modal.module.css'
 
-export default function Modal ({
-  children,
-  title,
-  icon,
-  showOnClose,
-  backdrop,
-  dialog,
-  notCloseable
-}) {
+/**
+ * The container of a Modal.
+ * @param {object} param React params.
+ * @param {(JSX.Element | undefined)[] | JSX.Element | null} param.children React content.
+ * @param {string}       param.title   The title of the modal dialog.
+ * @param {JSX.Element?} param.icon   Icon that will be displayed in the header.
+ * @param {boolean?}     param.showCloseButton   Show a close icon in the header.
+ * @param {object}       param.dialog  Reakit dialog state.
+ * @param {boolean?}     param.notCloseable  If true then this dialog can not be closed by clicking outside.
+ */
+export default function Modal ({ children, title, icon, showCloseButton, dialog, notCloseable }) {
   const closeable = !notCloseable
 
-  const closeIconInHeader = showOnClose && closeable
+  const closeIconInHeader = showCloseButton && closeable
     ? (
       <button
-        className={'closeModal ' + styles.CloseButton}
+        className='p-0 bg-transparent border-0 rounded focus:outline-none focus:shadow-outline'
         onClick={event => {
           event.preventDefault()
           dialog.hide()
@@ -31,27 +31,30 @@ export default function Modal ({
     : <span />
 
   return (
-    <>
-      {backdrop && (
-        <Portal>
-          <DialogBackdrop {...dialog} className={styles.Background} />
-        </Portal>
-      )}
+    <DialogBackdrop
+      {...dialog}
+      className='fixed top-0 left-0 z-50 flex items-center justify-center w-screen h-screen bg-black bg-opacity-50'
+    >
       <Dialog
         {...dialog}
-        className={styles.Border}
+        className='relative z-50 flex flex-col max-h-screen mx-auto text-gray-900 bg-white rounded'
         aria-label={title}
         hideOnEsc={closeable}
         hideOnClickOutside={closeable}
       >
-        <div className={styles.Header}>
+        <div className='flex flex-row-reverse justify-between mx-1 mt-1 mb-0 border-b border-black'>
           {closeIconInHeader}
-          <h4 className={styles.Title}>{icon}{title}</h4>
+
+          <div className='flex flex-row ml-2 mr-5'>
+            {icon}
+            <h4 className='my-2 ml-1'>{title}</h4>
+          </div>
         </div>
-        <article className={styles.Content}>
+
+        <article className='relative flex flex-col m-4 overflow-y-scroll overscroll-y-contain'>
           {children}
         </article>
       </Dialog>
-    </>
+    </DialogBackdrop>
   )
 }
