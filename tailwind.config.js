a9b750d35c1b7df86ea24a2d435640b10a2a942d
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-09-07 23:09:25 +0200

    chore(tailwind): activate all future breaking changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
   purge: [
     './public/**/*.html',
     './src/components/**/*.js',
