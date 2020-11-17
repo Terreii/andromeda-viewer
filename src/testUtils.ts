0f14406c9a995e5d610c981fe77c87666eefdc4a
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-11-17 23:05:53 +0100

    test(testUtils): add createTestStore with diff functions

---
+++ src/testUtils.ts
@@ -0,0 +1,43 @@
+import { diff as getObjDiff } from 'deep-object-diff'
+
+import { createExtraArgument, createStoreCore } from './store/configureStore'
+
+/**
+ * Create a store and some helper functions for testing actions.
+ */
+export function createTestStore ({ localDB, remoteDB }: {
+  localDB: PouchDB.Database,
+  remoteDB?: PouchDB.Database
+}) {
+  const extraArgument = createExtraArgument(localDB, remoteDB)
+  const store = createStoreCore(undefined, extraArgument)
+
+  const initialState = store.getState()
+  const states = new Map<string, ReturnType<typeof store.getState>>()
+
+  // Saves the current state unter that key for later comparison
+  const setMark = (key: string) => {
+    states.set(key, store.getState())
+  }
+
+  // Get the diff from one state to another (default the current)
+  const getDiff = (startKey?: string, endKey?: string) => {
+    if (startKey && !states.has(startKey)) {
+      throw new Error('unknown start key ' + startKey)
+    }
+    if (endKey && !states.has(endKey)) {
+      throw new Error('unknown end key ' + endKey)
+    }
+
+    const oldState = startKey ? states.get(startKey)! : initialState
+    const newerState = endKey ? states.get(endKey)! : store.getState()
+    return getObjDiff(oldState, newerState)
+  }
+
+  return {
+    store,
+    cryptoStore: extraArgument.cryptoStore,
+    setMark,
+    getDiff
+  }
+}
