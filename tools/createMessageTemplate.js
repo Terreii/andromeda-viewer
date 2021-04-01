fe26bccfcafc53737d6b9e128b4b25ac62de2692
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-04-01 22:37:02 +0200

    refactor(doc): rename doc folder and move configs

--- tools/createMessageTemplate.js
+++ tools/createMessageTemplate.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 // parse the message_template.msg and creates the templates for the messages
 // It is implemented as a loader for Webpack
@@ -32,49 +32,55 @@ const path = require('path')
 //   }
 // ]
 
-const templatePath = path.resolve('tools', 'message_template.msg')
-const messageTemplateData = fs.readFileSync(templatePath, 'utf8')
+const templatePath = path.resolve('tools', 'message_template.msg');
+const messageTemplateData = fs.readFileSync(templatePath, 'utf8');
 
-const parsedTemplateString = parseMessageTemplate(messageTemplateData)
-const outPath = path.resolve('src', 'network', 'messages.json')
-fs.writeFileSync(outPath, parsedTemplateString, 'utf8')
+const parsedTemplateString = parseMessageTemplate(messageTemplateData);
+const outPath = path.resolve('src', 'network', 'messages.json');
+fs.writeFileSync(outPath, parsedTemplateString, 'utf8');
 
-function parseMessageTemplate (data) {
-  const allMessages = data.split('\n').map(line => {
-    // remove the commens
-    return line.replace(/\/\/.*$/, '').replace(/^\s+$/g, '')
-  }).filter(line => {
-    return line.length !== 0 && line !== 'version 2.0'
-  }).reduce(combineBlocks, {
-    finished: [], // All finished messages will be stored here
-    thisMessage: null // The message that is currently parsed is stored here
-  }).finished.map(parseMessage)
+function parseMessageTemplate(data) {
+  const allMessages = data
+    .split('\n')
+    .map((line) => {
+      // remove the commens
+      return line.replace(/\/\/.*$/, '').replace(/^\s+$/g, '');
+    })
+    .filter((line) => {
+      return line.length !== 0 && line !== 'version 2.0';
+    })
+    .reduce(combineBlocks, {
+      finished: [], // All finished messages will be stored here
+      thisMessage: null, // The message that is currently parsed is stored here
+    })
+    .finished.map(parseMessage);
 
-  return JSON.stringify(allMessages)
+  return JSON.stringify(allMessages);
 }
 
-function combineBlocks (combined, line) { // combine the blocks together
+function combineBlocks(combined, line) {
+  // combine the blocks together
   switch (line.charAt(0)) {
     case '{': // start a new block
-      combined.thisMessage = []
-      break
+      combined.thisMessage = [];
+      break;
     case '}': // the block is done
-      combined.finished.push(combined.thisMessage)
-      break
+      combined.finished.push(combined.thisMessage);
+      break;
     default:
-      combined.thisMessage.push(line)
-      break
+      combined.thisMessage.push(line);
+      break;
   }
-  return combined
+  return combined;
 }
 
-function parseMessage (message) {
+function parseMessage(message) {
   // parse a message
-  const head = message[0].trim().split(/\s+/g)
+  const head = message[0].trim().split(/\s+/g);
   const body = message.slice(1).reduce(parseBlocks, {
     all: [],
-    thisBlock: null
-  }).all
+    thisBlock: null,
+  }).all;
   return {
     name: head[0],
     frequency: head[1],
@@ -82,39 +88,39 @@ function parseMessage (message) {
     trusted: head[3] === 'Trusted',
     zerocoded: head[4] === 'Zerocoded',
     isOld: head[5],
-    body: body
-  }
+    body: body,
+  };
 }
 
-function parseBlocks (blocks, line) {
-  const trimed = line.trim()
+function parseBlocks(blocks, line) {
+  const trimed = line.trim();
   if (trimed.length === 1 && trimed.charAt(0) === '{') {
     blocks.thisBlock = {
       name: '',
       quantity: '',
       times: 0,
-      variables: []
-    }
+      variables: [],
+    };
   } else if (trimed.length === 1 && trimed.charAt(0) === '}') {
-    blocks.all.push(blocks.thisBlock)
+    blocks.all.push(blocks.thisBlock);
   } else if (trimed.charAt(0) !== '{') {
     // block info has no { at the beginning
-    const info = trimed.split(/\s+/g)
-    blocks.thisBlock.name = info[0]
-    blocks.thisBlock.quantity = info[1]
-    blocks.thisBlock.times = +info[2]
+    const info = trimed.split(/\s+/g);
+    blocks.thisBlock.name = info[0];
+    blocks.thisBlock.quantity = info[1];
+    blocks.thisBlock.times = +info[2];
   } else if (trimed.charAt(0) === '{' && trimed.length > 2) {
     // all variables have the fromat { name type quantity? }
-    const variable = trimed.split(/\s+/g)
-    let type = variable[2]
+    const variable = trimed.split(/\s+/g);
+    let type = variable[2];
     if (type === 'Variable') {
-      type += variable[3].trim()
+      type += variable[3].trim();
     }
     blocks.thisBlock.variables.push({
       name: variable[1],
       type: type,
-      times: +variable[3]
-    })
+      times: +variable[3],
+    });
   }
-  return blocks
+  return blocks;
 }
--- tools/updateFiles.js
+++ tools/updateFiles.js
@@ -1,52 +1,54 @@
-'use strict'
+'use strict';
 
-const fs = require('fs')
-const path = require('path')
-const util = require('util')
-const fetch = require('node-fetch')
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const fetch = require('node-fetch');
 
-const writeFile = util.promisify(fs.writeFile)
+const writeFile = util.promisify(fs.writeFile);
 
-const repositoryURL = 'https://bitbucket.org/lindenlab/viewer-release/raw/tip/'
+const repositoryURL = 'https://bitbucket.org/lindenlab/viewer-release/raw/tip/';
 
 fetch(repositoryURL + 'indra/newview/llviewerregion.cpp')
-
-  .then(response => {
+  .then((response) => {
     if (response.status === 200) {
-      return response.text()
+      return response.text();
     }
-    throw new Error(`${response.status} - ${response.statusText}`)
+    throw new Error(`${response.status} - ${response.statusText}`);
   })
 
-  .then(file => file
-    .split('\n')
-    .filter(line => line.includes('capabilityNames.append'))
-    .map(line => line.split('"')[1]) // capabilityNames.append("EventQueueGet"); -> EventQueueGet
+  .then(
+    (file) =>
+      file
+        .split('\n')
+        .filter((line) => line.includes('capabilityNames.append'))
+        .map((line) => line.split('"')[1]) // capabilityNames.append("EventQueueGet"); -> EventQueueGet
   )
 
-  .then(caps => {
-    const capsJSON = JSON.stringify(caps, null, 2)
-    const outPath = path.resolve('src', 'actions', 'capabilities.json')
-    return writeFile(outPath, capsJSON + '\n')
+  .then((caps) => {
+    const capsJSON = JSON.stringify(caps, null, 2);
+    const outPath = path.resolve('src', 'actions', 'capabilities.json');
+    return writeFile(outPath, capsJSON + '\n');
   })
 
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
 fetch(repositoryURL + 'scripts/messages/message_template.msg')
-
-  .then(response => {
+  .then((response) => {
     if (response.status < 300) {
-      return response.text()
+      return response.text();
     }
-    throw new Error(`${response.status} - ${response.statusText}`)
+    throw new Error(`${response.status} - ${response.statusText}`);
   })
 
-  .then(file => writeFile(path.resolve('tools', 'message_template.msg'), file))
+  .then((file) =>
+    writeFile(path.resolve('tools', 'message_template.msg'), file)
+  )
 
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
